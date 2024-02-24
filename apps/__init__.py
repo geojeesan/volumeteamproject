9615732315576619bcfecf5d571fb7adc583438c
TallowCatch <ameer3204@gmail.com>
2024-02-24 15:48:51 +0400

    Added comments to show PEP-8 style

--- apps/__init__.py
+++ apps/__init__.py
@@ -2,7 +2,7 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
+#Relevant imports
 import os
 
 from flask import Flask
@@ -30,13 +30,14 @@ def configure_database(app):
 
     @app.before_first_request
     def initialize_database():
+        """ Create all tables before the first request if they don't exist. """
         try:
             db.create_all()
         except Exception as e:
 
             print('> Error: DBMS Exception: ' + str(e) )
 
-            # fallback to SQLite
+            # Fallback to SQLite if there is an exception
             basedir = os.path.abspath(os.path.dirname(__file__))
             app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
 
@@ -45,17 +46,19 @@ def configure_database(app):
 
     @app.teardown_request
     def shutdown_session(exception=None):
+        """ Remove the database session after each request. """
         db.session.remove()
 
 from apps.authentication.oauth import github_blueprint
 
 def create_app(config):
+    """ Application factory to create Flask app instances with given configs. """
     app = Flask(__name__)
     app.config.from_object(config)
     register_extensions(app)
     register_blueprints(app)
-
+    # Register the GitHub OAuth blueprint with a URL prefix
     app.register_blueprint(github_blueprint, url_prefix="/login") 
-    
+    # Configure and initialize the database
     configure_database(app)
     return app
