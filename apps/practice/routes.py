31ae4d4541ebef5ea47b1d7cb32d9d60700e8891
hassan-204 <92609713+hassan-204@users.noreply.github.com>
2024-02-28 15:22:20 +0400

    New practice page along with backend related functions

--- apps/practice/routes.py
+++ apps/practice/routes.py
@@ -7,12 +7,29 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+from flask import jsonify
 
 from apps.config import API_GENERATOR
 
 @blueprint.route('/practice')
 def practice():
     return render_template('practice/practice.html', segment='practice', API_GENERATOR=len(API_GENERATOR))
+
+
+def get_speech_score(num):
+    return num - 10
+
+
+@blueprint.route('/analyze_speech', methods=['POST'])
+def analyze_speech():
+
+    requestData = request.get_json()  # Retrieve requestData from the POST request
+
+    # Use the requestData as needed
+    # For example, pass it to get_speech_score function
+    num = requestData['num']
+
+    return jsonify(get_speech_score(num))
 
 
 # Helper - Extract current page name from request
