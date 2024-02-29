5677139d97b690d28e5208362ef19c69ee618681
sajia balkees <sajia@MacBook-Air-3.local>
2024-02-29 12:16:18 +0400

    Added feedback page

--- apps/__init__.py
+++ apps/__init__.py
@@ -21,7 +21,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home', 'api', 'practice', 'lessons'):
+    for module_name in ('authentication', 'home', 'api', 'practice', 'lessons', 'feedback'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint, name = module_name)
 
---
+++ apps/feedback/routes.py
@@ -0,0 +1,71 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps.home import blueprint
+from flask import render_template, request
+from flask_login import login_required
+from jinja2 import TemplateNotFound
+from flask import jsonify
+
+from apps.config import API_GENERATOR
+
+@blueprint.route('/feedback')
+def feedback():
+    return render_template('feedback/feedback.html', segment='feedback', API_GENERATOR=len(API_GENERATOR))
+
+
+def get_speech_score(num):
+    return num - 10
+
+
+
+def send_to_database(feedback):
+    # Some code which sends feedback to database
+
+    sent = False
+
+    return sent
+
+
+
+@blueprint.route('/send_feedback', methods=['POST'])
+def send_feedback():
+
+    requestData = request.get_json()  # Retrieve requestData from the POST request
+
+    # Use the requestData as needed
+    # For example, pass it to get_speech_score function
+    feedback = requestData['feedback']
+
+    has_been_sent = send_to_database(feedback)
+
+    if has_been_sent:
+        return jsonify("Your feedback has been sent")
+    else:
+        return jsonify("Failed to send feedback")
+
+
+    # to_return = f"Your feedback '{feedback}' has been submitted"
+
+    to_return = f"Your feedback has been submitted"
+
+
+    return jsonify(to_return)
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
