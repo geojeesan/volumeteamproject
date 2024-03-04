00de7bd35f859e8a23d4e035117feceb231cbb68
geojeesan <81578090+geojeesan@users.noreply.github.com>
2024-03-04 10:32:23 +0400

    Added Privacy Policy, Changes to Footer and Profile Page

---
+++ apps/privacypolicy/routes.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps.home import blueprint
+from flask import render_template, request
+from flask_login import login_required
+from jinja2 import TemplateNotFound
+
+from apps.config import API_GENERATOR
+
+@blueprint.route('/privacypolicy')
+def privacypolicy():
+    return render_template('privacypolicy/privacypolicy.html', segment='privacypolicy', API_GENERATOR=len(API_GENERATOR))
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
