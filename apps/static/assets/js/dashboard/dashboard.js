4cd5fd17b0cdd7d516d86ac059849294a591b023
Maha Adwan <mahafady2412@gmail.com>
2024-03-16 22:13:17 +0400

    Finished alot of the cards

--- apps/static/assets/js/dashboard/dashboard.js
+++ apps/static/assets/js/dashboard/dashboard.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const endDate = new Date(event.end_utc);
       
         const eventElement = document.createElement('div');
-        eventElement.className = 'd-flex.align-items-start.mb-3'; // This class is now more specific in the CSS
+        eventElement.className = 'd-flex align-items-start timeline-event-spacing';
         eventElement.innerHTML = `
         <div class="card-body p-3">
           <div class="timeline timeline-one-side">
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', function() {
               </span>
               <div class="timeline-content">
                 <h6 class="text-dark text-sm font-weight-bold mb-0">${event.name}</h6>
-                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"> Date: ${startDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '-')}, Time: ${startDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })} - ${endDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
-                <p class="text-secondary text-xs mt-1 mb-0">${event.description}</p>
+                <p class="text-secondary font-weight-bold text-xs mt-0 mb-0"> Date: ${startDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '-')}, Time: ${startDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })} - ${endDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
+                <p class="text-secondary text-xs mt-0 mb-0">${event.description}</p>
           </div>
         </div>
         `;
@@ -37,100 +37,122 @@ document.addEventListener('DOMContentLoaded', function() {
     Chart.defaults.font.family = 'Roboto'; // Set the font family globally
     Chart.defaults.color = '#353B3C'; // Set the global font color
 
-    const progressCtx = document.getElementById('progressChart').getContext('2d');
-    const progressChart = new Chart(progressCtx, {
-      type: 'bar',
-      data: {
-        labels: ['Clear Diction', 'Emotional Expression', 'Tone Consistency', 'Pace Regulation'],
-        datasets: [{
-          label: 'Current Progress',
-          data: [65, 59, 80, 81], // Replace these numbers with your actual data
-          backgroundColor: '#D56565',
-          borderWidth: 0,
-          borderRadius: 10, // Rounded corners for the bars (Chart.js 3.x and newer)
-        }, {
-          label: 'Goal',
-          data: [85, 90, 100, 100], // Replace these numbers with your actual data
-          backgroundColor: '#BADFF2',
-          borderWidth: 0,
-          borderRadius: 10, // Rounded corners for the bars (Chart.js 3.x and newer)
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            ticks: {
-              font: {
-                size: 14, // You can adjust the size here
-              },
-              color: '#353B3C', // Y-axis ticks color
-            }
-          },
-          x: {
-            ticks: {
-              font: {
-                size: 14, // You can adjust the size here
-              },
-              color: '#353B3C', // X-axis ticks color
-            }
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            labels: {
-              font: {
-                size: 14, // Legend font size
-              },
-              color: '#353B3C', // Legend font color
-            }
-          },
-        }
-      }
-    });
+    document.addEventListener('DOMContentLoaded', function() {
+      var scoresCtx = document.getElementById('testScoresChart').getContext('2d');
+      var scoresChart;
+  
+      // Fetch the scores data from the Flask route
+      fetch('/test-scores')
+          .then(response => response.json())
+          .then(data => {
+              scoresChart = new Chart(scoresCtx, {
+                  type: 'line',
+                  data: data,
+                  options: {
+                      scales: {
+                          y: {
+                              beginAtZero: true,
+                              suggestedMax: 10 // Ensure y-axis goes from 0 to 10
 
-const scoresCtx = document.getElementById('testScoresChart').getContext('2d');
-const scoresChart = new Chart(scoresCtx, {
-  type: 'line',
-  data: {
-    labels: ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5'],
-    datasets: [{
-      label: 'Test Scores',
-      data: [0, 2, 3, 5, 10],
-      fill: false,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+                          }
+                      },
+                      responsive: true,
+                      maintainAspectRatio: false
+                  }
+              });
+          })
+          .catch(error => console.error('Error loading test scores:', error));
+  });
+  
+document.addEventListener('DOMContentLoaded', function() {
+  var stickerElement = document.getElementById('dynamicSticker');
+  var images = [
+    'interview.svg',
+    'microphone.svg',
+    'speech-bubble.svg',
+    'agree.svg',
+  ];
+
+  // Calculate the index based on the current day
+  var daysSinceEpoch = Math.floor((new Date() - new Date(2024, 1, 1)) / (1000 * 60 * 60 * 24));
+  var imageIndex = daysSinceEpoch % images.length;
+
+  // Construct the full URL for the image
+  var imageUrl = 'static/assets/img/logos/' + images[imageIndex];
+  stickerElement.src = imageUrl;
+  console.log(imageUrl)
+
+});
+// add to routes.py 
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Fetch user progress after DOM content is loaded
+  fetch('/user-progress')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
       }
-    },
-    responsive: true,
-    maintainAspectRatio: false
-  }
+      return response.json();
+    })
+    .then(data => {
+
+      // Update placeholders with user progress data
+      console.log(data)
+
+      document.getElementById('lessons-completed').textContent = data.lessons_completed;
+      document.getElementById('lessons-in-progress').textContent = data.lessons_in_progress;
+      //document.getElementById('current-level').textContent = data.current_level;
+      document.getElementById('level_progress').textContent = data.level_progress.toFixed(0) + '%'; // toFixed(0) for no decimal places
+      // If total practice hours is implemented, update it here
+      // document.getElementById('total-practice-hours').textContent = data.total_practice_hours;
+    })
+    .catch(error => {
+      console.error('Error fetching user progress:', error);
+      // Handle errors by showing user feedback or a placeholder
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  var images = [
-    url_for('static', filename='img/logos/interview.svg'),
-    url_for('static', filename='img/logos/microphone.svg'),
-    url_for('static', filename='img/logos/speech-bubble.svg'),
-    url_for('static', filename='img/logos/agree (1).svg'),
-  ];
+  fetch('/leaderboard')
+  .then(response => response.json())
+  .then(leaderboardData => {
+    const leaderboardBody = document.getElementById('leaderboard-body');
+    leaderboardBody.innerHTML = ''; // Clear previous content
 
-  // Choose a random image from the array
-  var randomImage = images[Math.floor(Math.random() * images.length)];
+    const badges = [
+      '/static/assets/img/goldBadge.svg',   // For 1st place
+      '/static/assets/img/silverBadge.svg', // For 2nd place
+      '/static/assets/img/bronzeBadge.svg'  // For 3rd place
+    ];
 
-  // Find the image by ID and change its source to the random one
-  var imgElement = document.getElementById('dynamicSticker');
-  if(imgElement) {
-    imgElement.src = randomImage;
-  } else {
-    console.error('No element with the ID dynamicSticker was found.');
-  }
-}); 
+    // Ensure we always have 6 rows in the leaderboard
+    const numberOfRows = 6;
+    for (let i = 0; i < numberOfRows; i++) {
+      const row = leaderboardBody.insertRow();
+      
+      const rankCell = row.insertCell();
+      // Check if the entry exists and if it's in the top 3 to assign a badge
+      if (leaderboardData[i] && i < 3) {
+        rankCell.innerHTML = `<div class="badge-container">
+                                <img src="${badges[i]}" alt="Badge" class="badge-icon">
+                                <span class="badge-number">${i + 1}</span>
+                              </div>`;
+      } else {
+        rankCell.textContent = leaderboardData[i] ? leaderboardData[i].rank : i + 1;
+      }
+
+      const usernameCell = row.insertCell();
+      usernameCell.textContent = leaderboardData[i] ? leaderboardData[i].username : '-';
+
+      const scoreCell = row.insertCell();
+      scoreCell.textContent = leaderboardData[i] ? leaderboardData[i].score : '-';
+
+      const levelCell = row.insertCell();
+      levelCell.textContent = leaderboardData[i] ? leaderboardData[i].level : '-';
+
+      const progressCell = row.insertCell();
+      progressCell.textContent = leaderboardData[i] ? leaderboardData[i].level_progress + '%' : '-';
+    }
+  })
+  .catch(error => console.error('Error loading leaderboard:', error));
+});
