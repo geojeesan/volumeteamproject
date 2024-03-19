686e6da0952315edf8271f6470d592ce1e507474
Maha Adwan <mahafady2412@gmail.com>
2024-03-19 15:37:01 +0400

    Added persona card and recent activity

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-
 import random
 from apps.home import blueprint
 from flask import render_template, request
@@ -21,198 +20,133 @@ from apps.models import (
 )
 from apps.authentication.models import Users
 from sqlalchemy.sql.expression import func
-from sqlalchemy import and_, case
 
 
+# Helper function to calculate daily index
 def get_daily_index(total_items, start_date=datetime(2024, 1, 1)):
     days_since_start = (datetime.utcnow() - start_date).days
     return days_since_start % total_items
 
-
-@blueprint.route("/index")
-@login_required
-def index():
-    # Get the current day's featured lesson
-    lessons = Lesson.query.all()
-    lesson_index = get_daily_index(len(lessons), datetime(2024, 1, 1))
-    featured_lesson = lessons[lesson_index]
-
-    # Fetch the images for the featured lesson from lesson_images table
-    featured_images = LessonImage.query.filter_by(lesson_id=featured_lesson.id).all()
-
-    # Construct the full URLs for the images
-    featured_image_urls = [
-        "/static/assets/img/" + image.image_path for image in featured_images
-    ]
-
-    return render_template(
-        "home/index.html",
-        segment="index",
-        API_GENERATOR=len(API_GENERATOR),
-        featured_lesson=featured_lesson,
-        image_url=featured_image_urls[0],  # Pass only the first image URL to the template
-    )
-
-
-# Test scores chart
-@blueprint.route("/test-scores")
-@login_required
-def test_scores():
-    scores_query = (
-        UserScenarioProgress.query.order_by(UserScenarioProgress.score.asc())
-        .limit(6)
-        .all()
-    )
-
-    scores_data = {
-        # Generate labels dynamically based on the number of entries in scores_query
-        "labels": [f"Test {i+1}" for i in range(len(scores_query))],
-        "datasets": [
-            {
-                "label": "Test Scores",
-                "data": [score.score for score in scores_query],
-                "fill": False,
-                "borderColor": "rgb(75, 192, 192)",
-                "tension": 0.1,
-            }
-        ],
-    }
-    return jsonify(scores_data)
-
-
-# 3 cards on top
+# User Progress Endpoint
 @blueprint.route("/user-progress")
 @login_required
 def user_progress():
     user_id = current_user.get_id()
+    user_progress = UserProgress.query.filter_by(user_id=user_id).first()
+    return jsonify({
+        "lessons_completed": user_progress.lessons_completed,
+        "lessons_in_progress": user_progress.lessons_in_progress,
+        "current_level": user_progress.current_level,
+        "level_progress": user_progress.level_progress,
+        "streak": user_progress.streak,
+    })
 
+# Volume persona
+@blueprint.route("/user-persona")
+@login_required
+def user_persona():
+    user_id = current_user.get_id()
     user_progress = UserProgress.query.filter_by(user_id=user_id).first()
 
-    if not user_progress:
-        UserProgress.create_new_progress(user_id=user_id)
-        user_progress = UserProgress.query.filter_by(user_id=user_id).first()
+    # Integrate the user_rank logic here
+    leaderboard = db.session.query(
+        UserScenarioProgress.user_id,
+        func.max(UserScenarioProgress.score).label("best_score"),
+        Users.username,
+    ).join(Users, UserScenarioProgress.user_id == Users.id).group_by(UserScenarioProgress.user_id).order_by(func.max(UserScenarioProgress.score).desc()).all()
+    
+    rank = None
+    for idx, user in enumerate(leaderboard):
+        if user.user_id == int(user_id):
+            rank = idx + 1
+            break
 
-    lessons_completed = user_progress.lessons_completed
-    lessons_in_progress = user_progress.lessons_in_progress
-    current_level = user_progress.current_level
-    level_progress = user_progress.level_progress
-    streak = user_progress.streak  # Fetch the streak from the database
+    persona_data = {
+        "level": user_progress.current_level,
+        "streak": user_progress.streak,
+        "rank": rank
 
-    return jsonify(
-        {
-            "lessons_completed": lessons_completed,
-            "lessons_in_progress": lessons_in_progress,
-            "current_level": current_level,
-            "level_progress": level_progress,
-            "streak": streak,  # Include the streak in the response
-        }
-    )
+    }
+    return jsonify(persona_data)
 
-
-# leaderboard
-@blueprint.route("/leaderboard")
+# Personal Notes Endpoints
+@blueprint.route('/get-notes', methods=['GET'])
 @login_required
-def leaderboard():
-    top_users = (
-        db.session.query(
-            UserScenarioProgress.user_id,
-            func.max(UserScenarioProgress.score).label("best_score"),
-            Users.username,
-        )
-        .join(Users, UserScenarioProgress.user_id == Users.id)
-        .group_by(UserScenarioProgress.user_id)
-        .order_by(func.max(UserScenarioProgress.score).desc())
-        .limit(6)
-        .all()
-    )
-    leaderboard_data = []
-    for idx, user in enumerate(top_users):
-        user_progress = UserProgress.query.filter_by(user_id=user.user_id).first()
-        if not user_progress:
-            UserProgress.create_new_progress(user_id=user.user_id)
-            user_progress = UserProgress.query.filter_by(user_id=user.user_id).first()
+def get_notes():
+    user_id = current_user.get_id()
+    note = UserNotes.query.filter_by(user_id=user_id).first_or_404()
+    return jsonify({"content": note.content})
 
-        current_level = user_progress.current_level
-        level_progress = user_progress.level_progress
-        leaderboard_data.append(
-            {
-                "rank": idx + 1,
-                "username": user.username,
-                "score": user.best_score,
-                "level": current_level,
-                "level_progress": level_progress,
-            }
-        )
-
-    return jsonify(leaderboard_data)
-
-
-#user notes 
 @blueprint.route('/save-notes', methods=['POST'])
 @login_required
 def save_notes():
     content = request.json.get('content', '')
     user_id = current_user.get_id()
     note = UserNotes.query.filter_by(user_id=user_id).first()
-
     if note:
         note.content = content
     else:
-        new_note = UserNotes(user_id=user_id, content=content)
-        db.session.add(new_note)
-
+        db.session.add(UserNotes(user_id=user_id, content=content))
     db.session.commit()
     return jsonify({"message": "Notes saved successfully"})
 
-@blueprint.route('/get-notes', methods=['GET'])
-@login_required
-def get_notes():
-    user_id = current_user.get_id()
-    note = UserNotes.query.filter_by(user_id=user_id).first_or_404()
-
-    return jsonify({"content": note.content})
-
-
-
-#recent activity 
+# Recent Activity Endpoint
 @blueprint.route("/user-actions")
 @login_required
 def user_actions():
     user_id = current_user.get_id()
-    # Limit to the 5 most recent actions
     actions = UserActionLog.query.filter_by(user_id=user_id).order_by(UserActionLog.timestamp.desc()).limit(5).all()
-
-    actions_data = [
-        {"action": action.action, "timestamp": action.timestamp.strftime("%Y-%m-%d %H:%M:%S")}
-        for action in actions
-    ]
-
+    actions_data = [{"action": action.action, "timestamp": action.timestamp.strftime("%Y-%m-%d %H:%M:%S")} for action in actions]
     return jsonify(actions_data)
 
-# upcoming events
+# Test Scores Over Time Endpoint
+@blueprint.route("/test-scores")
+@login_required
+def test_scores():
+    scores_query = UserScenarioProgress.query.order_by(UserScenarioProgress.score.asc()).limit(6).all()
+    scores_data = {
+        "labels": [f"Test {i+1}" for i in range(len(scores_query))],
+        "datasets": [{
+            "label": "Test Scores",
+            "data": [score.score for score in scores_query],
+            "fill": False,
+            "borderColor": "rgb(75, 192, 192)",
+            "tension": 0.1,
+        }]
+    }
+    return jsonify(scores_data)
+
+# Leaderboard Endpoint
+@blueprint.route("/leaderboard")
+@login_required
+def leaderboard():
+    top_users = db.session.query(
+        UserScenarioProgress.user_id,
+        func.max(UserScenarioProgress.score).label("best_score"),
+        Users.username,
+    ).join(Users, UserScenarioProgress.user_id == Users.id).group_by(UserScenarioProgress.user_id).order_by(func.max(UserScenarioProgress.score).desc()).limit(10).all()
+    leaderboard_data = [{"rank": idx + 1, "username": user.username, "score": user.best_score, "level": UserProgress.query.filter_by(user_id=user.user_id).first().current_level, "level_progress": UserProgress.query.filter_by(user_id=user.user_id).first().level_progress} for idx, user in enumerate(top_users)]
+    return jsonify(leaderboard_data)
+
+# Upcoming Events Endpoint
 @blueprint.route("/index/upcoming-events")
 @login_required
 def upcoming_events():
     current_time = datetime.utcnow()
-    events = (
-        Event.query.filter(Event.end_utc > current_time)
-        .order_by(Event.start_utc.asc())
-        .limit(5)
-        .all()
-    )
-    events_data = [
-        {
-            "id": event.id,
-            "name": event.name,
-            "description": event.description,
-            "start_utc": event.start_utc.strftime("%Y-%m-%d %H:%M:%S"),
-            "end_utc": event.end_utc.strftime("%Y-%m-%d %H:%M:%S"),
-        }
-        for event in events
-    ]
-
+    events = Event.query.filter(Event.end_utc > current_time).order_by(Event.start_utc.asc()).limit(4).all()
+    events_data = [{"id": event.id, "name": event.name, "description": event.description, "start_utc": event.start_utc.strftime("%Y-%m-%d %H:%M:%S"), "end_utc": event.end_utc.strftime("%Y-%m-%d %H:%M:%S")} for event in events]
     return jsonify(events_data)
 
+# Featured Lesson Endpoint
+@blueprint.route("/index")
+@login_required
+def index():
+    lessons = Lesson.query.all()
+    lesson_index = get_daily_index(len(lessons))
+    featured_lesson = lessons[lesson_index]
+    featured_images = LessonImage.query.filter_by(lesson_id=featured_lesson.id).all()
+    featured_image_urls = ["/static/assets/img/" + image.image_path for image in featured_images]
+    return render_template("home/index.html", segment="index", API_GENERATOR=len(API_GENERATOR), featured_lesson=featured_lesson, image_url=featured_image_urls[0])
 
 @blueprint.route("/<template>")
 def route_template(template):
--- apps/static/assets/js/dashboard/dashboard.js
+++ apps/static/assets/js/dashboard/dashboard.js
@@ -1,260 +1,151 @@
-// Events widget 
 document.addEventListener('DOMContentLoaded', function() {
-
-  fetch('/index/upcoming-events')
-    .then(response => response.json())
-    .then(events => {
-      const eventsContainer = document.getElementById('events-container');
-      eventsContainer.innerHTML = ''; // Clear previous events
-      events.forEach(event => {
-        const startDate = new Date(event.start_utc);
-        const endDate = new Date(event.end_utc);
-      
-        const eventElement = document.createElement('div');
-        eventElement.className = 'd-flex align-items-start timeline-event-spacing';
-        eventElement.innerHTML = `
-        <div class="card-body p-3">
-          <div class="timeline timeline-one-side">
-            <div class="timeline-block mb-0">
-              <span class="timeline-step">
-                <i class="ni ni-bell-55 text-success text-gradient"></i>
-              </span>
-              <div class="timeline-content">
-                <h6 class="text-dark text-sm font-weight-bold mb-0">${event.name}</h6>
-                <p class="text-secondary font-weight-bold text-xs mt-0 mb-0"> Date: ${startDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '-')}, Time: ${startDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })} - ${endDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
-                <p class="text-secondary text-xs mt-0 mb-0">${event.description}</p>
-          </div>
-        </div>
-        `;
-        eventsContainer.appendChild(eventElement);
+  // User Progress: Displays user's learning progress, lessons completed, and current level
+  fetch('/user-progress')
+      .then(response => response.json())
+      .then(data => {
+        
+          document.getElementById('lessons-completed').textContent = data.lessons_completed;
+          document.getElementById('lessons-in-progress').textContent = data.lessons_in_progress;
+          document.getElementById('level_progress').textContent = data.level_progress.toFixed(0) + '%';
+      })
+      .catch(error => {
+          console.error('Error fetching user progress:', error);
       });
-    })
-    .catch(error => console.error('Error loading events:', error));
-});
 
-    document.addEventListener('DOMContentLoaded', function() {
-      var scoresCtx = document.getElementById('testScoresChart').getContext('2d');
-      var scoresChart;
-  
-      // Fetch the scores data from the Flask route
-      fetch('/test-scores')
-          .then(response => response.json())
-          .then(data => {
-              scoresChart = new Chart(scoresCtx, {
-                  type: 'line',
-                  data: data,
-                  options: {
-                      scales: {
-                          y: {
-                              beginAtZero: true,
-                              suggestedMax: 10 // Ensure y-axis goes from 0 to 10
-
-                          }
-                      },
-                      responsive: true,
-                      maintainAspectRatio: false
-                  }
-              });
-          })
-          .catch(error => console.error('Error loading test scores:', error));
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    fetch('/user-progress')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-
-            document.getElementById('lessons-completed').textContent = data.lessons_completed;
-            document.getElementById('lessons-in-progress').textContent = data.lessons_in_progress;
-            document.getElementById('level_progress').textContent = data.level_progress.toFixed(0) + '%';
-
-            // Clear previous content and create level badge
-            const levelBadgeContainer = document.getElementById('user-level-badge');
-            levelBadgeContainer.innerHTML = '';
-            const levelBadge = document.createElement('span');
-            levelBadge.className = `badge-card badge-card-${data.current_level}`;
-            levelBadge.textContent = data.current_level.charAt(0).toUpperCase() + data.current_level.slice(1);
-            levelBadgeContainer.appendChild(levelBadge);
-
-            // Clear previous content and create streak badge with icon
-            const streakBadgeContainer = document.getElementById('user-streak-badge');
-            streakBadgeContainer.innerHTML = '';
-            const streakBadge = document.createElement('span');
-            streakBadge.className = 'badge-card badge-card-streak';
-            streakBadgeContainer.appendChild(streakBadge);
-
-          
-
-            // Create and append streak text to streak badge
-            const streakText = document.createTextNode(` Streak: ${data.streak}`);
-            streakBadge.appendChild(streakText);
-
-            const fireIcon = document.createElement('i');
-            fireIcon.className = 'fas fa-fire';
-            streakBadge.appendChild(fireIcon);
-        })
-        .catch(error => {
-            console.error('Error fetching user progress:', error);
-        });
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('/leaderboard')
+  fetch('/user-persona')
     .then(response => response.json())
-    .then(leaderboardData => {
-      const leaderboardBody = document.getElementById('leaderboard-body');
-      leaderboardBody.innerHTML = ''; // Clear previous content
+    .then(personaData => {
+      // Update the content of each badge with the fetched data
+      document.getElementById('user-level-text').textContent += personaData.level;
+      document.getElementById('user-streak-text').textContent += personaData.streak;
+      document.getElementById('user-rank-text').textContent += personaData.rank;
 
-      // Ensure we always have 6 rows in the leaderboard
-      const numberOfRows = 6;
-      for (let i = 0; i < numberOfRows; i++) {
-        const row = leaderboardBody.insertRow();
-
-        // Assign rank to the rankCell
-        const rankCell = row.insertCell();
-        const rankText = leaderboardData[i] ? leaderboardData[i].rank : i + 1;
-        rankCell.textContent = rankText;
-
-        // Assign username to the usernameCell
-        const usernameCell = row.insertCell();
-        usernameCell.textContent = leaderboardData[i] ? leaderboardData[i].username : '-';
-
-        // Assign score to the scoreCell
-        const scoreCell = row.insertCell();
-        scoreCell.textContent = leaderboardData[i] ? leaderboardData[i].score : '-';
-
-        // Assign level to the levelCell
-        const levelCell = row.insertCell();
-        levelCell.textContent = leaderboardData[i] ? leaderboardData[i].level : '-';
-
-        // Assign progress to the progressCell
-        const progressCell = row.insertCell();
-        progressCell.textContent = leaderboardData[i] ? `${leaderboardData[i].level_progress}%` : '-';
-      }
     })
     .catch(error => {
-      console.error('Error loading leaderboard:', error);
+      console.error('Error fetching user persona data:', error);
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Fetch user progress and leaderboard data simultaneously
-  Promise.all([
-    fetch('/user-progress').then(response => response.json()),
-    fetch('/leaderboard').then(response => response.json())
-  ]).then(([userData, leaderboardData]) => {
-    // ... (handle the userData as before, including level and streak badges)
-    
-    // Process leaderboard data to find the current user's rank
-    const currentUserRankEntry = leaderboardData.find(entry => entry.username === userData.current_username);
-    const currentUserRank = currentUserRankEntry ? currentUserRankEntry.rank : '-';
-    
-    // Display rank badge
-    const rankBadgeContainer = document.getElementById('user-rank-badge');
-    rankBadgeContainer.innerHTML = ''; // Clear previous content
-
-    const rankBadge = document.createElement('span');
-    rankBadge.className = 'badge-card badge-card-rank';
-    rankBadge.textContent = `Rank: ${currentUserRank}`;
-    rankBadgeContainer.appendChild(rankBadge);
-
-    // Additional code for displaying 1st and 2nd rank badges, if applicable
-    if (currentUserRankEntry && currentUserRank <= 3) {
-      const rankBadgeImage = document.createElement('img');
-      rankBadgeImage.src = `/static/assets/img/${['gold', 'silver', 'bronze'][currentUserRank - 1]}Badge.svg`;
-      rankBadgeImage.alt = `Rank ${currentUserRank}`;
-      rankBadgeImage.className = 'badge-icon';
-      rankBadgeContainer.insertBefore(rankBadgeImage, rankBadge);
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
-});
-
-// dashboard.js
-
-document.addEventListener('DOMContentLoaded', function() {
+  // Personal Notes: Allows users to save and fetch personal notes
   const notesTextarea = document.getElementById('personal-notes');
   const saveNotesButton = document.getElementById('save-notes-btn');
 
-  // Function to fetch notes and display them
-  function fetchNotes() {
-    fetch('/get-notes')
+  fetch('/get-notes')
       .then(response => response.json())
       .then(data => {
-        notesTextarea.value = data.content;
+          notesTextarea.value = data.content;
       })
       .catch(error => console.error('Error fetching notes:', error));
-  }
 
-  // Function to save notes
-  function saveNotes() {
-    const content = notesTextarea.value;
-    fetch('/save-notes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        // Add any other necessary headers like authentication tokens
-      },
-      body: JSON.stringify({ content: content })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.message);
-      // Show a success message to the user if needed
-    })
-    .catch(error => {
-      console.error('Error saving notes:', error);
-      // Show an error message to the user if needed
-    });
-  }
-
-  // Load the notes when the page is loaded
-  fetchNotes();
-
-  // Add event listener to the save button
-  saveNotesButton.addEventListener('click', saveNotes);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchAndDisplayUserActions();
-});
-
-function fetchAndDisplayUserActions() {
-  fetch('/user-actions') // Adjust the endpoint as necessary
+  saveNotesButton.addEventListener('click', function() {
+      const content = notesTextarea.value;
+      fetch('/save-notes', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ content: content })
+      })
       .then(response => response.json())
-      .then(actions => {
-          const recentActivityContainer = document.getElementById('recent-activity-container');
-          recentActivityContainer.innerHTML = ''; // Clear previous content
-          actions.forEach(action => {
-              const actionElement = document.createElement('div');
-              actionElement.classList.add('d-flex', 'align-items-start', 'mb-2'); // Add spacing between items
-              actionElement.innerHTML = `
-            <div class="card-body p-3">
-              <div class="timeline timeline-one-side">
-                  <div class="timeline-block mb-0">
-                      <span class="timeline-step">
-                          <i class="fas fa-user-edit text-info"></i> <!-- Adjust icon as necessary -->
-                      </span>
-                      <div class="timeline-content">
-                          <h6 class="text-dark text-sm font-weight-bold mb-0">${action.action}</h6>
-                          <p class="text-secondary text-xs mt-1 mb-0">${action.timestamp}</p>
-                      </div>
-                  </div>
-                </div>
-              </div>
-              `;
-              recentActivityContainer.appendChild(actionElement);
+      .then(data => {
+          console.log(data.message);
+      })
+      .catch(error => {
+          console.error('Error saving notes:', error);
+      });
+  });
+
+  /// Recent Activity: Shows the user's recent actions within the platform
+  fetch('/user-actions')
+  .then(response => response.json())
+  .then(actions => {
+      const recentActivityContainer = document.getElementById('recent-activity-container');
+      actions.forEach(action => {
+          const actionElement = document.createElement('div');
+          actionElement.classList.add('activity-item'); // Personalized class for activity items
+          actionElement.innerHTML = `
+              <div class="activity-time-indicator"></div> <!-- Personalized class for time indicator -->
+              <div class="activity-content">
+                  <h6 class="activity-title">${action.action}</h6> <!-- Personalized class for titles -->
+                  <p class="activity-time">${action.timestamp}</p> <!-- Personalized class for timestamps -->
+              </div>`;
+          recentActivityContainer.appendChild(actionElement);
+      });
+  })
+  .catch(error => console.error('Error loading user actions:', error));
+
+
+  // Test Scores Over Time: Visualizes the user's test scores over a period
+  var scoresCtx = document.getElementById('testScoresChart').getContext('2d');
+  fetch('/test-scores')
+      .then(response => response.json())
+      .then(data => {
+          new Chart(scoresCtx, {
+              type: 'line',
+              data: data,
+              options: {
+                  scales: {
+                      y: {
+                          beginAtZero: true,
+                          suggestedMax: 10
+                      }
+                  },
+                  responsive: true,
+                  maintainAspectRatio: false
+              }
           });
       })
-      .catch(error => console.error('Error loading user actions:', error));
-}
+      .catch(error => console.error('Error loading test scores:', error));
+
+  // Leaderboard: Displays a leaderboard of top users based on scores
+  fetch('/leaderboard')
+      .then(response => response.json())
+      .then(leaderboardData => {
+          const leaderboardBody = document.getElementById('leaderboard-body');
+          for (let i = 0; i < 10; i++) { // Ensure 6 rows
+              const row = leaderboardBody.insertRow();
+              const rankCell = row.insertCell();
+              rankCell.textContent = leaderboardData[i] ? leaderboardData[i].rank : i + 1;
+              const usernameCell = row.insertCell();
+              usernameCell.textContent = leaderboardData[i] ? leaderboardData[i].username : '-';
+              const scoreCell = row.insertCell();
+              scoreCell.textContent = leaderboardData[i] ? leaderboardData[i].score : '-';
+              const levelCell = row.insertCell();
+              levelCell.textContent = leaderboardData[i] ? leaderboardData[i].level : '-';
+              const progressCell = row.insertCell();
+              progressCell.textContent = leaderboardData[i] ? `${leaderboardData[i].level_progress}%` : '-';
+          }
+      })
+      .catch(error => {
+          console.error('Error loading leaderboard:', error);
+      });
+
+  // Upcoming Events: Lists upcoming events that users might be interested in
+  fetch('/index/upcoming-events')
+      .then(response => response.json())
+      .then(events => {
+          const eventsContainer = document.getElementById('events-container');
+          events.forEach(event => {
+              const startDate = new Date(event.start_utc);
+              const endDate = new Date(event.end_utc);
+              const eventElement = document.createElement('div');
+              eventElement.className = 'd-flex align-items-start timeline-event-spacing';
+              eventElement.innerHTML = `
+              <div class="card-body p-1">
+                  <div class="timeline timeline-one-side">
+                      <div class="timeline-block mb-0">
+                          <span class="timeline-step">
+                              <i class="ni ni-bell-55 text-success text-gradient"></i>
+                          </span>
+                          <div class="timeline-content">
+                              <h6 class="text-dark text-sm font-weight-bold mb-0">${event.name}</h6>
+                              <p class="text-secondary font-weight-bold text-xs mt-0 mb-0"> Date: ${startDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '-')}, Time: ${startDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })} - ${endDate.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
+                              <p class="text-secondary text-xs mt-0 mb-0">${event.description}</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>`;
+              eventsContainer.appendChild(eventElement);
+          });
+      })
+      .catch(error => console.error('Error loading events:', error));
+});
