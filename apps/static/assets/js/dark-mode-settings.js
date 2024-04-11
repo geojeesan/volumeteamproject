0c6d1119923012279489dfef35f302c76c178497
hassan-204 <92609713+hassan-204@users.noreply.github.com>
2024-04-11 15:54:30 +0400

    Fixed dark mode load delay

--- apps/static/assets/js/dark-mode-settings.js
+++ apps/static/assets/js/dark-mode-settings.js
@@ -1,12 +1,13 @@
+// This JS script is to handle the specific elements on the page which relate to the settings 
+// depending on whether the user is dark mode or not
+
 const themeSwitch = document.getElementById("theme-switch");
 const themeIndicator = document.getElementById("theme-indicator");
-const page = document.body;
 
-const themeStates = ["light", "dark"]
+
 const indicators = ["fa-moon", "fa-sun"]
-const pageClass = ["bg-gray-100", "dark-page"]
 
-let currentTheme = localStorage.getItem("theme");
+
 
 function setTheme(theme) {
     localStorage.setItem("theme", themeStates[theme])
@@ -18,28 +19,22 @@ function setIndicator(theme) {
     themeIndicator.classList.add(indicators[theme])
 }
 
-function setPage(theme) {
-    page.classList.remove(pageClass[0])
-    page.classList.remove(pageClass[1])
-    page.classList.add(pageClass[theme])
-}
+
 
 
 if (currentTheme === null) {
     localStorage.setItem("theme", themeStates[0])
     setIndicator(0)
-    setPage(0)
     themeSwitch.checked = true;
 }
+
 if (currentTheme === themeStates[0]) {
     setIndicator(0)
-    setPage(0)
     themeSwitch.checked = true;
 
 }
 if (currentTheme === themeStates[1]) {
     setIndicator(1)
-    setPage(1)
     themeSwitch.checked = false;
 }
 
